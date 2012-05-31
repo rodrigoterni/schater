e0def6008e6d30bebef6cd4958787071ecd53487
Rodrigo Terni <rodrigo.terni@gmail.com>
2012-05-31 10:46:06 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Schater::Application.config.secret_token = '2ee852d93db4d5a5172bf47eb89e859d109f8201d50f1215bad13bba1299d3866cd8b9e828a79580ac9ecbf83c3c9a04c19a9762fa844047b5fe45f96994969d'
